7d7a2616b2c066c7743e5b87f6c81fc2a0acf5b8
KT-BERRY <abhishek080403@example.com>
2023-10-04 17:11:54 +0530

    second push

--- src/CameraPopup.js
+++ src/CameraPopup.js
@@ -71,37 +71,70 @@ const CameraPopup = ({ onClose, generateChatbotResponse, appendMessage }) => {
         canvas.height = video.videoHeight;
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
   
-        const imageSrcData = canvas.toDataURL('image/png');
-        setImageSrc(imageSrcData);
+        const img_path = canvas.toDataURL('image/jpg');
+        setImageSrc(img_path);
     }
 
-    if (canvas) {
-        const context = canvas.getContext('2d');
-        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    // if (canvas) {
+    //     const context = canvas.getContext('2d');
+    //     context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
   
-        const imageSrcData = canvas.toDataURL('image/png');
+    //     const imageSrcData = canvas.toDataURL('image/png');
         
-        // Send the captured image to the API for text recognition
-        try {
-          const response = await axios.post('https://demo.botaiml.com/card-ocr/image-ocr-extraction', {
-            image: imageSrcData,
-          });
+    //     // // Send the captured image to the API for text recognition
+    //     // try {
+    //     //   const response = await axios.post('https://demo.botaiml.com/card-ocr/image-ocr-extraction', {
+    //     //     image: imageSrcData,
+    //     //   });
   
-          // Extract text from the API response
-          const recognizedText = response.data.text;
+    //     //   // Extract text from the API response
+    //     //   const recognizedText = response.data.text;
   
-          // Pass the recognized text to your chatbot logic
-          const responseMessage = generateChatbotResponse(recognizedText);
+    //     //   // Pass the recognized text to your chatbot logic
+    //     //   const responseMessage = generateChatbotResponse(recognizedText);
   
-          // Update messages state
-          appendMessage('chatbot', responseMessage);
+    //     //   // Update messages state
+    //     //   appendMessage('chatbot', responseMessage);
   
-          setImageSrc(imageSrcData); // Show captured image
-        } catch (error) {
-          console.error('Error processing image:', error);
-        }
-      }
+    //     //   setImageSrc(imageSrcData); // Show captured image
+    //     // } catch (error) {
+    //     //   console.error('Error processing image:', error);
+    //     // }
+    //   }
   };
+
+  const handleSendToAPI = async () => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const context = canvas.getContext('2d');
+      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+      const img_path = canvas.toDataURL('image/jpg');
+
+      const imgBlob = await new Promise((resolve) => canvas.toBlob(resolve, 'image/jpeg')); // Change to JPEG format
+
+      const formData = new FormData();
+      formData.append('img_path', imgBlob, 'captured_image.jpg'); // Assuming the API expects 'img_path' as the key
+
+      try {
+        const response = await axios.post('https://demo.botaiml.com/card-ocr/image-ocr-extraction', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          image: img_path
+        });
+
+        const recognizedText = response.data.text;
+        const responseMessage = generateChatbotResponse(recognizedText);
+        appendMessage('chatbot', responseMessage);
+
+        setImageSrc(img_path); // Show captured image
+      } catch (error) {
+        console.error('Error processing image:', error);
+      }
+    }
+  }
 
   const startCamera = async () => {
     try {
@@ -138,16 +171,17 @@ const CameraPopup = ({ onClose, generateChatbotResponse, appendMessage }) => {
         {imageSrc ? (
           <>
             <img src={imageSrc} alt="Captured" style={{ maxWidth: '100%', height: 'auto' }}/>
-            <button onClick={handleSaveImage}>Save Image</button>
-            <button onClick={handleRetakeImage}>Retake Image</button>
+            <button onClick={handleSaveImage}>Save</button>
+            <button onClick={handleRetakeImage}>Retake</button>
+            <button onClick={handleSendToAPI}>Send</button> {/* New button */}
           </>
         ) : (
           <video ref={videoRef} autoPlay playsInline />
         )}
         {/* {imageSrc && <img src={imageSrc} alt="Uploaded" style={{ maxWidth: '100%', height: 'auto' }} />} */}
         {!imageSrc && 
-        <button onClick={handleCaptureImage}>Capture Image</button>}
-        <button onClick={handleUploadButtonClick}>Upload Image</button>
+        <button onClick={handleCaptureImage}>Capture</button>}
+        <button onClick={handleUploadButtonClick}>Upload</button>
         {/* <input type="file" accept="image/*" onChange={handleFileUpload} /> */}
         <input
             type="file"
